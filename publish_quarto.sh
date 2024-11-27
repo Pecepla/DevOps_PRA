9095b30ff733ef68d5b52d5f8001d45fbf76ef34
Alexkano10 <alex40806@gmail.com>
2024-11-27 12:26:23 +0100

    change script

--- publish_quarto.sh
+++ publish_quarto.sh
@@ -10,14 +10,17 @@ fi
 git add .
 git commit -m "$1"
 
-# Push the changes to the main branch
-git push origin main
+# Push the changes to the correct branch (MF03-PRA3-AlexCano)
+git push origin MF03-PRA3-AlexCano
+
+# Go to the 'mi-practica-quarto' folder where the Quarto project is located
+cd mi-practica-quarto
 
 # Publish to GitHub Pages using quarto
 quarto publish gh-pages --no-render --no-prompt
 
+# Log the message to a log file
 log_message() {
     local message="$1"
     local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
     echo "[${timestamp}] ${message}" >> log.txt
-}
